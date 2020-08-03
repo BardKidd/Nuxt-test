85482f35fda77ca53086d3ccd7a8c37295f55068
Lin Pei Wen <mobilesuit0922@gmail.com>
2020-08-03 21:13:54 +0800

    Nuxt.js 練習

---
+++ components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header id="header">
+    <div class="inner">
+      <!-- Logo -->
+      <nuxt-link to="/" href="index.html" class="logo">
+        <span class="symbol">
+          <img src="~assets/images/logo.svg" alt />
+        </span>
+        <span class="title">Ko No Dio Da</span>
+      </nuxt-link>
+
+      <!-- Nav -->
+      <nav>
+        <ul>
+          <li>
+            <a href="#menu">Menu</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
---
+++ components/Menu.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav id="menu">
+    <h2>Menu</h2>
+    <ul>
+      <li>
+        <nuxt-link to="/generic">generic</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/generic">generic</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/generic">generic</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/generic">generic</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/generic">generic</nuxt-link>
+      </li>
+    </ul>
+  </nav>
+</template>
